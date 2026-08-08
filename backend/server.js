89af88c42fa4e5acb942b8b24145b923c798ccb9
Sherli <sherliimran57@gmail.com>
2026-08-08 21:35:54 +0800

    Update backend server for deployment

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 // =========================================
